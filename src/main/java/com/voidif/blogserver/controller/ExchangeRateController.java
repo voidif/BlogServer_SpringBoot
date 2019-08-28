a76bd859fcf5649be57c00703fec0345a789adf1
Yifu Wang <w529399971@gmail.com>
2019-08-27 17:03:35 -0700

    rate controller bug fixed

--- src/main/java/com/voidif/blogserver/controller/ExchangeRateController.java
+++ src/main/java/com/voidif/blogserver/controller/ExchangeRateController.java
@@ -1,12 +1,14 @@
 package com.voidif.blogserver.controller;
 
 import com.voidif.blogserver.model.ExchangeRate;
+import com.voidif.blogserver.tool.ExchangeRateTool;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExchangeRateController {
     public ExchangeRate getExchangeRate(@RequestParam String currencyPairName) {
-        return null;
+        float rate = ExchangeRateTool.getExchangeRateByCurrencyPairName(currencyPairName);
+        return new ExchangeRate(currencyPairName, rate);
     }
 }
